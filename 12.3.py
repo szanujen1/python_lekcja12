a21814c0c3104a6a5d162d90e62d5eac7b34614d
unknown <maksple1337@gmail.com>
2020-01-02 12:05:25 +0100

    zestaw poprawiony

--- 12.3.py
+++ 12.3.py
@@ -1,13 +1,14 @@
 def mediana_sort(L, left, right):
-    L = sorted(L)
-    if len(L) % 2 == 1:
-        return L[int((left + right) / 2)]
+    result = L[left:right + 1]
+    # print(result)
+    if len(result) % 2 == 1:
+        return result[int((left + right) / 2)]
     else:
-        return (L[int((left + right) / 2)] + L[int((left + right) / 2) + 1]) / 2
+        return (result[int((left + right) / 2)] + result[int((left + right) / 2) + 1]) / 2
 
 
 L = [2, 1, 7, 8, 11, 0, 66]
-print(mediana_sort(L, 0, len(L) - 1))
+print(mediana_sort(L, 0, 4))
 
 L = [2, 1, 7, 8, 11, 0, 66, 77]
 print(mediana_sort(L, 0, len(L) - 1))
